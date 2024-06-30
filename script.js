adc1e9f53d014879d5a9cb92b9a309682358699e
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-07-01 01:03:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,69 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  let tableContainer = document.getElementById('myTable');
+  //Write your code here
+  let table = document.getElementById("sampleTable");
 
+  let insert_Row = table.insertRow(0);
 
-let table = document.createElement('table');
+  let newCell1 = insert_Row.insertCell(0);
+  let newCell2 = insert_Row.insertCell(1);
 
-// let thead = document.createElement('thead');
-// table.append(thead);
-
-// let thead = table.createElement('thead'); // Error
-
-let thead = table.createTHead();
-
-// add tr inside table head:
-
-// let tr1 = document.createElement('tr');
-// thead.append(tr1);
-
-let tr1 = thead.insertRow();
-
-let theadData = ['ID','Name', 'Age'];
-for(let t of theadData){
-     let th = document.createElement('th');
-     th.innerHTML = t;
-     tr1.append(th);
-
-    // insertCell() method is used to insert a cell in the current row.
-    // let th = tr1.insertCell();
-    // th.innerHTML = t;
-}
-
-
-function addRow(rowData){
-    //  let tbody  =  table.querySelector('tbody')
-    //  if(!tbody){
-    //     tbody = table.createTBody();
-    //  }
-    let  tbody = table.querySelector('tbody') || table.createTBody();
-    
-    let tr = tbody.insertRow();
-    for(let t of rowData){
-    //    insertCell() method is used to insert a cell in the current row.
-       let td = tr.insertCell();
-       td.innerHTML = t;
-   }
-        
-}
-
-addRow([1,'John', 25]);
-addRow([2,'Doe', 30]);
-addRow([3,'Smith', 35]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-tableContainer.append(table);
-  
+  newCell1.innerHTML = "New Cell1";
+  newCell2.innerHTML = "New Cell2";
 }
